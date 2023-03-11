fcbed6c9dd3457751b8dfc211c28ef8227806349
nicolas74 <nicolas.savoini@mac.com>
2023-03-11 15:35:43 -0500

    Initial Commit

---
+++ SwiftUiComponentDemo/SwiftUiComponentDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiComponentDemoApp.swift
+//  SwiftUiComponentDemo
+//
+//  Created by Nicolas Savoini on 2023-03-11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiComponentDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
